3a6247036d8a44af9c2751c893aa05fcd4b8a2f1
dldush <leecarus0301@naver.com>
2025-01-20 13:13:00 +0900

    FEAT: 개발 초기환경 세팅

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className={"w-[10px] bg-red-100"}>다음</Button>
+    </>
+  );
+}
+
+export default App;
