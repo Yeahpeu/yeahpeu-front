72d8a272d1115cf34afdfba68c90cece7c6969af
ze-eun <howubeeen@naver.com>
2025-01-22 17:02:28 +0900

    [FEAT] 캘린더 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,9 @@
 import "./App.css";
-import { useState } from "react";
-import MyButton from "./components/MyButton";
-import MyCheckButton from "./components/MyCheckButton";
-import MyConfirm from "./components/MyConfirm";
-import MyRoleButton from "./components/MyRole";
-import HomeProgressMolecule from "./molecules/HomeProgressMolecule";
-import OnboardProgressMolecule from "./molecules/OnboardingProgressMolecule";
-import MySearchBox from "./components/MySearchBar";
 
 function App() {
-  const marriagePercent = 77;
-  const progressPercent = 25;
-  const [selectedRole, setSelectedRole] = useState(null);
-
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleComplete = () => {
-    setIsCompleted((prev) => !prev);
-  };
-
-  const toggleAccordion = () => {
-    setIsOpen((prev) => !prev);
-  };
-
   return (
     <>
-      <HomeProgressMolecule marriagePercent={marriagePercent} />
-      <br />
-      <OnboardProgressMolecule
-        subject={"weddingHall"}
-        progressPercent={progressPercent}
-      />
-      <br />
-      <MyCheckButton value={"결혼식장 정하기"} />
-      <br />
-      <MyButton value={"다음"} color={"abled"} />
-      <MyButton value={"다음"} color={"disabled"} />
-      <MyConfirm
-        message={"테스트 해보는 중입니다"}
-        optionLeft={"취소"}
-        optionRight={"선택"}
-      />
-      <MyRoleButton selectedRole={selectedRole} onClick={setSelectedRole} />
-      <MySearchBox value={""} onChange={"onChange"} />
+      <div className=""></div>
     </>
   );
 }
---
+++ src/components/MyEmptyCard.jsx
@@ -0,0 +1,13 @@
+import emptyImg from "../assets/emptybox.png";
+
+const MyEmptyCard = ({ value }) => {
+  return (
+    <div className="flex flex-col justify-center">
+      <img src={emptyImg} className="w-[100%]" />
+      <div className="text-[400%] font-bold"> 텅 </div>
+      {value}
+    </div>
+  );
+};
+
+export default MyEmptyCard;
